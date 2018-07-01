2bef1a9a63cc4f8faf46c1638656060dcd7bc205
freqn <ceyler@gmail.com>
2018-07-01 12:51:36 -0400

    adds nav helper specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,7 +59,7 @@ RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers, type: :feature
   config.include FactoryGirl::Syntax::Methods
   Capybara.javascript_driver = :poltergeist
-  Capybara.server = :puma 
+  Capybara.server = :puma  
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
